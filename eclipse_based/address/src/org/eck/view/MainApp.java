156c3e611512fd609ff1d8cf3403b313fe528ea0
Erick Zanardo <erickzanardoo@gmail.com>
2013-11-20 19:31:14 -0200

    ''

---
+++ eclipse_based/address/src/org/eck/view/MainApp.java
@@ -0,0 +1,64 @@
+package org.eck.view;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
+
+		try {
+			// Load the root layout from the fxml file
+			FXMLLoader loader = new FXMLLoader(MainApp.class.getClassLoader().getResource("org/eck/view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			// Exception gets thrown if the fxml file could not be loaded
+			e.printStackTrace();
+		}
+
+		showPersonOverview();
+	}
+
+	/**
+	 * Returns the main stage.
+	 * 
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	/**
+	 * Shows the person overview scene.
+	 */
+	public void showPersonOverview() {
+		try {
+			// Load the fxml file and set into the center of the main layout
+			FXMLLoader loader = new FXMLLoader(MainApp.class.getClassLoader().getResource("org/eck/view/PersonOverview.fxml"));
+			AnchorPane overviewPage = (AnchorPane) loader.load();
+			rootLayout.setCenter(overviewPage);
+
+		} catch (IOException e) {
+			// Exception gets thrown if the fxml file could not be loaded
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
